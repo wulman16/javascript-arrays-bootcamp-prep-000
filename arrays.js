7b1e6de67c6049272651d5db8e5e76c6c2b859a4
William Ulman <will.ulman@gmail.com>
2019-01-03 21:22:11 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(array, element) {
+  var newArray = [element, ...array];
+  return newArray;
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  var newArray = array.unshift(element);
+  return newArray;
+}
