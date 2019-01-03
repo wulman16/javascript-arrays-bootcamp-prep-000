56edd61ceb76fb0ffdc23c4db44bffec6c37f2ff
William Ulman <will.ulman@gmail.com>
2019-01-03 21:24:56 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -9,3 +9,12 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element);
   return array;
 }
+
+function addElementToEndOfArray(array, element) {
+  var newArray = [...array, element];
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array;
+}
